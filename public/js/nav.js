6c50187446d45120b994f884d858e257848e6e55
Sonia <coleysonia@gmail.com>
2023-06-12 19:53:41 -0400

    p

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,18 +1,19 @@
 const createNav = () => {
     let nav = document.querySelector('.navbar');
-    nav.innerHTML= `
+
+    nav.innerHTML = `
         <div class="nav">
             <img src="img/dark-logo.png" class="brand-logo" alt="">
             <div class="nav-items">
                 <div class="search">
                     <input type="text" class="search-box" placeholder="search brand, product">
-                    <button class="search-btn"> Search </button>
+                    <button class="search-btn">search</button>
                 </div>
                 <a>
                     <img src="img/user.png" id="user-img" alt="">
                     <div class="login-logout-popup hide">
-                        <p class="account-info"><Log in as, name</p>
-                        <button class="btn" id="user-btn">Log Out</button>
+                        <p class="account-info">Log in as, name</p>
+                        <button class="btn" id="user-btn">Log out</button>
                     </div>
                 </a>
                 <a href="#"><img src="img/cart.png" alt=""></a>
@@ -20,18 +21,17 @@ const createNav = () => {
         </div>
         <ul class="links-container">
             <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">Women</a></li>
-            <li class="link-item"><a href="#" class="link">Men</a></li>
-            <li class="link-item"><a href="#" class="link">Kids</a></li>
-            <li class="link-item"><a href="#" class="link">Accessories</a></li>
+            <li class="link-item"><a href="#" class="link">women</a></li>
+            <li class="link-item"><a href="#" class="link">men</a></li>
+            <li class="link-item"><a href="#" class="link">kids</a></li>
+            <li class="link-item"><a href="#" class="link">accessories</a></li>
         </ul>
     `;
 }
+
 createNav();
 
-
-//nav popup
-
+// nav popup
 const userImageButton = document.querySelector('#user-img');
 const userPopup = document.querySelector('.login-logout-popup');
 const popuptext = document.querySelector('.account-info');
@@ -41,12 +41,22 @@ userImageButton.addEventListener('click', () => {
     userPopup.classList.toggle('hide');
 })
 
-window.onload= () => {
-    let user = JSSON.parse(sessionStorage.user || null);
+window.onload = () => {
+    let user = JSON.parse(sessionStorage.user || null);
     if(user != null){
-        //means user is logged in
+        // means user is logged in
         popuptext.innerHTML = `log in as, ${user.name}`;
-    }else {
-        //user is logged out
+        actionBtn.innerHTML = 'log out';
+        actionBtn.addEventListener('click', () => {
+            sessionStorage.clear();
+            location.reload();
+        })
+    } else{
+        // user is logged out
+        popuptext.innerHTML = 'log in to place order';
+        actionBtn.innerHTML = 'log in';
+        actionBtn.addEventListener('click', () => {
+            location.href = '/login';
+        })
     }
 }
--- public/js/token.js
+++ public/js/token.js
@@ -2,7 +2,7 @@ let char = `123abcde.fmnopqlABCDE@FJKLMNOPQRSTUVWXYZ456789stuvwxyz0!#$%&ijkrgh'*
 
 const generateToken = (key) => {
     let token = '';
-    for(let i = 0; i < key.length; i=i++){
+    for(let i = 0; i < key.length; i++){
         let index = char.indexOf(key[i]) || char.length / 2;
         let randomIndex = Math.floor(Math.random() * index);
         token += char[randomIndex] + char[index - randomIndex];
@@ -12,13 +12,56 @@ const generateToken = (key) => {
 
 const compareToken = (token, key) => {
     let string = '';
-    for (let i = 0; i < token.length; i=i+2){
-       let index1 = char.indexOf(token[i]); 
-       let index2 = char.indexOf(token[i+1]);
-       string += char[index1 + index2];
+    for(let i = 0; i < token.length; i=i+2){
+        let index1 = char.indexOf(token[i]);
+        let index2 = char.indexOf(token[i+1]);
+        string += char[index1 + index2];
     }
     if(string === key){
         return true;
     }
     return false;
+}
+
+//  common functions
+
+// send data function
+const sendData = (path, data) => {
+    fetch(path, {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify(data)
+    }).then((res) => res.json())
+    .then(response => {
+        processData(response);
+    })
+}
+
+const processData = (data) => {
+    loader.style.display = null;
+    if(data.alert){
+        showAlert(data.alert);
+    } else if(data.name){
+        // create authToken
+        data.authToken = generateToken(data.email);
+        sessionStorage.user = JSON.stringify(data);
+        location.replace('/');
+    } else if(data == true){
+        // seller page
+        let user = JSON.parse(sessionStorage.user);
+        user.seller = true;
+        sessionStorage.user = JSON.stringify(user);
+        location.reload();
+    }
+}
+
+// alert function
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000);
 }
