003270df424f522a779718b5786d84aa3b02ece4
Sonia <coleysonia@gmail.com>
2023-06-13 00:07:04 -0400

    p

--- server.js
+++ server.js
@@ -12,6 +12,46 @@ admin.initializeApp({
   });
   
   let db = admin.firestore();
+
+  //aws config 
+  const aws = require('aws-sdk');
+  const dotenv = require('dotenv');
+  
+  dotenv.config();
+
+  //aws parameters
+  const region = "us-east-2";
+  const bucketName = "ecommerce-website-01"
+  const accessKeyId = process.env.AWS_ACCESS_KEY;
+  const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+  aws.config.update({
+    region,
+    accessKeyId,
+    secretKeyId
+  })
+
+  //init s3
+  const s3 = new aws.s3();
+  
+  //generate image upload link
+  async function generateUrl(){
+    let date = new Date();
+    let id = parseInt(Math.random() * 10000000000);
+
+    const imageName = `${id}${date.getTime()}.jpg`;
+
+    const params = ({
+        Bucket: bucketName,
+        Key : imageName,
+        Expires: 300, //300 ms
+        ContentType: 'image/jpeg'
+    }) 
+    const uploadUrl = await s3.getSignedUrlPromise('putObject', params);
+    return uploadUrl;
+  }
+
+
   
   // declare static path
   let staticPath = path.join(__dirname, "public");
@@ -132,6 +172,95 @@ admin.initializeApp({
           })
       }
   })
+  
+  
+  
+  // add product
+app.get('/add-product', (req, res) => {
+    res.sendFile(path.join(staticPath, "addProduct.html"));
+})
+
+app.get('/add-product/:id', (req, res) => {
+    res.sendFile(path.join(staticPath, "addProduct.html"));
+})
+
+// get the upload link
+app.get('/s3url', (req, res) => {
+    generateUrl().then(url => res.json(url));
+})
+
+// add product
+app.post('/add-product', (req, res) => {
+    let { name, shortDes, des, images, sizes, actualPrice, discount, sellPrice, stock, tags, tac, email, draft, id } = req.body;
+
+    // validation
+    if(!draft){
+        if(!name.length){
+            return res.json({'alert': 'enter product name'});
+        } else if(shortDes.length > 100 || shortDes.length < 10){
+            return res.json({'alert': 'short description must be between 10 to 100 letters long'});
+        } else if(!des.length){
+            return res.json({'alert': 'enter detail description about the product'});
+        } else if(!images.length){ // image link array
+            return res.json({'alert': 'upload atleast one product image'})
+        } else if(!sizes.length){ // size array
+            return res.json({'alert': 'select at least one size'});
+        } else if(!actualPrice.length || !discount.length || !sellPrice.length){
+            return res.json({'alert': 'you must add pricings'});
+        } else if(stock < 20){
+            return res.json({'alert': 'you should have at least 20 items in stock'});
+        } else if(!tags.length){
+            return res.json({'alert': 'enter few tags to help ranking your product in search'});
+        } else if(!tac){
+            return res.json({'alert': 'you must agree to our terms and conditions'});
+        } 
+    }
+
+    // add product
+    let docName = id == undefined ? `${name.toLowerCase()}-${Math.floor(Math.random() * 5000)}` : id;
+    db.collection('products').doc(docName).set(req.body)
+    .then(data => {
+        res.json({'product': name});
+    })
+    .catch(err => {
+        return res.json({'alert': 'some error occured. Try again'});
+    })
+})
+
+// get products
+app.post('/get-products', (req, res) => {
+    let { email, id } = req.body;
+    let docRef = id ? db.collection('products').doc(id) : db.collection('products').where('email', '==', email);
+
+    docRef.get()
+    .then(products => {
+        if(products.empty){
+            return res.json('no products');
+        }
+        let productArr = [];
+        if(id){
+            return res.json(products.data());
+        } else{
+            products.forEach(item => {
+                let data = item.data();
+                data.id = item.id;
+                productArr.push(data);
+            })
+            res.json(productArr)
+        }
+    })
+})
+
+app.post('/delete-product', (req, res) => {
+    let { id } = req.body;
+    
+    db.collection('products').doc(id).delete()
+    .then(data => {
+        res.json('success');
+    }).catch(err => {
+        res.json('err');
+    })
+})
   
   // 404 route
   app.get('/404', (req, res) => {
